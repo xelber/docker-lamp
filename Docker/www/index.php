cdd9cc06c104bbdcce313d444463b59c0798a063
Hiran Milinda <hiranmilinda@gmail.com>
2019-09-16 11:27:53 +1000

    Link reletively

--- Docker/www/index.php
+++ Docker/www/index.php
@@ -1,3 +1,13 @@
-<?php
-
-echo phpinfo();
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html lang="en">
+<head>
+    <meta http-equiv="content-type" content="text/html; charset=utf-8">
+    <title>Docker LAMP</title>
+</head>
+<body>
+    <p><a href="/index.php">PHP Info</a></p>
+    <p><a href="<?php echo $_SERVER['SERVER_NAME']?>:8081">Adminer</a></p>
+    <p><a href="<?php echo $_SERVER['SERVER_NAME']?>:8082">PHPMyAdmin</a></p>
+    <p><a href="<?php echo $_SERVER['SERVER_NAME']?>:8083">Mongo Express</a></p>
+</body>
+</html>
